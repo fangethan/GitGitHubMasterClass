97f2d29faf2c79b6c977d05c6930c2a1b41d1552
Ethan <Ethan>
2020-12-04 19:19:59 +1100

    my second commit

--- Main.java
+++ Main.java
@@ -6,5 +6,6 @@ public class Main {
 	// write your code here
         System.out.println("hello");
         // my github token: 0c43efcb8c4e10ce9bba28de11a7aa401f66a2ac
+        //remind Ethan to save the token
     }
 }
