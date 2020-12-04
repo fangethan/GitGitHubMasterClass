2ca29807820c56ada4c5adec20a515cd4d765599
Ethan <Ethan>
2020-12-04 19:17:24 +1100

    first commit from ethan

--- Main.java
+++ Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("hello");
+        // my github token: 0c43efcb8c4e10ce9bba28de11a7aa401f66a2ac
     }
 }
