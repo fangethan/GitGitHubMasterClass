18baee04bc5a712fd53395d860a0d34aa9f8f05a
fangethan <37927289+fangethan@users.noreply.github.com>
2020-12-05 12:33:24 +1100

    update main.java
    add some code from github

--- Main.java
+++ Main.java
@@ -7,5 +7,7 @@ public class Main {
         System.out.println("hello");
         // my github token: 0c43efcb8c4e10ce9bba28de11a7aa401f66a2ac
         //remind Ethan to save the token
+	System.out.println("hello again");
+
     }
 }
